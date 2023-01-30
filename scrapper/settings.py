9ab1abb9f12792017b7a76ed8fcdd23065d469ca
Arturo Navarro Riveros <arnariv@outlook.com>
2023-01-30 17:52:58 -0500

    For deploy
    Signed-off-by: Arturo Navarro Riveros <arnariv@outlook.com>

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -80,22 +80,22 @@ DATABASES = {
     #    "ENGINE": "django.db.backends.sqlite3",
     #    "NAME": BASE_DIR / "db.sqlite3",
     #}
-    #"default": {
-    #    "ENGINE": "django.db.backends.mysql",
-    #    "NAME": "arturoalmaquinta_pruebas",
-    #    "USER": "arturoalmaquinta_admin",
-    #    "PASSWORD": "gamer001!",
-    #    "HOST": "localhost",
-    #    "PORT": "3306",
-    #}
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "trabajos",
-        "USER": "root",
-        "PASSWORD": "",
+        "NAME": "arturoalmaquinta_pruebas",
+        "USER": "arturoalmaquinta_admin",
+        "PASSWORD": "gamer001!",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "3306",
     }
+    #"default": {
+    #    "ENGINE": "django.db.backends.mysql",
+    #    "NAME": "trabajos",
+    #    "USER": "root",
+    #    "PASSWORD": "",
+    #    "HOST": "localhost",
+    #    "PORT": "",
+    #}
 }
 
 
